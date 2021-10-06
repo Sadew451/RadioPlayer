21e82bdc032c3b2918c17b02725403331a9e730f
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-10-06 18:52:29 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -1,21 +1,3 @@
-"""
-RadioPlayerV2, Telegram Voice Chat Userbot
-Copyright (C) 2021  Sadew Jayasekara
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
-
 from pyrogram import Client, filters
 
 import youtube_dl
@@ -95,7 +77,7 @@ def a(client, message):
         m.delete()
         message.delete()
     except Exception as e:
-        m.edit('**An Error Occured. Please Report This To @SafoTheBot !!**')
+        m.edit('**An Error Occured. Please Report This To @SDBOTz !!**')
         print(e)
     try:
         os.remove(audio_file)
