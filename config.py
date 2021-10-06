06d3a8fda29662be5da899c7391ae6c2060f008c
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-10-06 19:32:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,31 +1,13 @@
-"""
-RadioPlayerV2, Telegram Voice Chat Userbot
-Copyright (C) 2021  Sadew Jayasekara <https://t.me/Darkridersslk>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
-
 import os
 import re
 
 
 class Config:
-    ADMIN = os.environ.get("ADMINS", "1803810116 1221558981")
+    ADMIN = os.environ.get("ADMINS", "1548967589 1391755824")
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     API_ID = int(os.environ.get("API_ID", 6775322))
-    CHAT = int(os.environ.get("CHAT", "-1001311942992"))
-    LOG_GROUP = os.environ.get("LOG_GROUP", "-1001311942992")
+    CHAT = int(os.environ.get("CHAT", "-1001130413114"))
+    LOG_GROUP = os.environ.get("LOG_GROUP", "-1001475839216")
     if LOG_GROUP:
         LOG_GROUP = int(LOG_GROUP)
     else:
