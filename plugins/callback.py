bff6b1a969d231d6bc72655eb56d08c501d0eaab
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-10-06 18:53:46 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -152,7 +152,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         except:
             pass
     elif query.data=="help":
-        await query.edit_message_text("🙋‍♂️ **Hi Bruh**, \nJust Send Me An Audio File To Play. You Can Use @EzilaXBot To Get Audio Files! 😌\n\nCheck /help To Know More ...",
+        await query.edit_message_text("🙋‍♂️ **Hi Bruh**, \nJust Send Me An Audio File To Play. You Can Use @TheNatsukiBot To Get Audio Files! 😌\n\nCheck /help To Know More ...",
         reply_markup=InlineKeyboardMarkup(
                 [
                     [
